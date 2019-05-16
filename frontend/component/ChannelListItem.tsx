8fa8bc84e5b2d32ae07e45dae582d9645c40afac
Rauli Laine <rauli.laine@iki.fi>
2019-05-16 22:49:48 +0300

    Style channel list a bit
    Color non-active channels with secondary color (unless they have unread
    messages) and use classnames library for more convenient CSS class name
    list generation.

--- frontend/component/ChannelListItem.tsx
+++ frontend/component/ChannelListItem.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import * as React from 'react';
 
 export interface Props {
@@ -7,19 +8,39 @@ export interface Props {
   onSelect: () => void;
 }
 
+const getLinkClassName = (props: Props) => classNames(
+  'p-2',
+  'd-block',
+  'text-secondary',
+  'text-decoration-none',
+  {
+    'bg-white': props.isActive,
+    'text-body': props.isActive,
+  },
+);
+
+const getChannelNameClassName = (props: Props) => classNames({
+  'font-weight-bold': props.hasUnreadMessages,
+  'text-body': props.isActive,
+  'text-white': !props.isActive && props.hasUnreadMessages,
+  'text-secondary': !props.isActive && !props.hasUnreadMessages,
+});
+
 const ChannelListItem: React.SFC<Props> = (props) => (
   <li>
     <a
       href="#"
-      className={`p-2 d-block text-decoration-none\
- ${props.isActive ? 'bg-white text-body' : 'text-light'}\
- ${props.hasUnreadMessages ? 'font-weight-bold' : ''}`}
+      className={getLinkClassName(props)}
       onClick={(event) => {
         event.preventDefault();
         props.onSelect();
       }}
     >
-      {props.name}
+      #
+      &nbsp;
+      <span className={getChannelNameClassName(props)}>
+        {props.name.substring(1)}
+      </span>
     </a>
   </li>
 );
